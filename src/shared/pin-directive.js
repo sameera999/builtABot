b91f466a3bc1939803049a16e19eb897e5d5cb29
samPerera999 <srasanga999@gmail.com>
2021-08-24 16:55:25 +0530

    Directive lify cycle hooks, use bind and update LCH with single functionality

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,10 +1,22 @@
-export default {
-  bind: (element, binding) => {
-    const el = element;
-    Object.keys(binding.value).forEach((position) => {
-      el.style[position] = binding.value[position];
-    });
+// export default {
+//   bind: (element, binding) => {
+//     const el = element;
+//     Object.keys(binding.value).forEach((position) => {
+//       el.style[position] = binding.value[position];
+//     });
 
-    el.style.position = 'absolute';
-  },
-};
+//     el.style.position = 'absolute';
+//   },
+
+// };
+
+// most common way to apply both 'bind' and 'update'
+// lifecyclehooks
+export default function (element, binding) {
+  const el = element;
+  Object.keys(binding.value).forEach((position) => {
+    el.style[position] = binding.value[position];
+  });
+
+  el.style.position = 'absolute';
+}
