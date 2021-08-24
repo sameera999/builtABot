3398d302ccd84dea0abc4accc872d1ee3661a235
samPerera999 <srasanga999@gmail.com>
2021-08-24 17:13:49 +0530

    Creating global currency filter

--- src/main.js
+++ src/main.js
@@ -8,11 +8,15 @@ import store from './store';
 // directives
 import pinDirective from './shared/pin-directive';
 
+// filters
+import currencyFilter from './shared/currency-filter';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false;
 Vue.directive('pin', pinDirective);
+Vue.filter('currency', currencyFilter);
 
 Vue.use(BootstrapVue);
 new Vue({
