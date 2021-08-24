28ae089ff99d5bf45eaf09eb1cf4ca8f9eb60e84
samPerera999 <srasanga999@gmail.com>
2021-08-24 16:59:59 +0530

    make 'pin' directive available globally

--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// directives
+import pinDirective from './shared/pin-directive';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false;
+Vue.directive('pin', pinDirective);
+
 Vue.use(BootstrapVue);
 new Vue({
   render: (h) => h(App),
